cfd79c453d5669166a53ec223dd0662e26e407d9
tekSchool <tekschool001@gmail.com>
2019-06-09 11:49:02 -0400

    GitPRacticeDemo class added in project

---
+++ src/main/java/Framework/TestNG/GitPracticeDemo.java
@@ -0,0 +1,12 @@
+package Framework.TestNG;
+
+public class GitPracticeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is repository practice");
+
+	}
+
+}
